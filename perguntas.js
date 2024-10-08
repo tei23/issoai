3a19f14eb3ff7d36adc344338b84896044258447
tei23 <159156789+tei23@users.noreply.github.com>
2024-10-08 12:19:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O  que é JavaScript?',
+    'O JavaScript é uma linguadem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O  que é uma função?',
+    'Uma função é um bloco de códigos que executam uma função'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual o resultado de 6 x 8 ?',
+    'O resultado é 48'
+)
